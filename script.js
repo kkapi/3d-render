afc2902ce6d0d9dc6cc6fbb12c6ba8156f10951c
kkapi <hZG2fik_j8@mail.ru>
2023-06-21 16:49:12 +0300

    input range camera distance

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const example = document.getElementById("example");
+const canvas = document.getElementById("example");
 const ctx = example.getContext('2d');
 
 const PIXEL_SIZE = 4;
 const CENTER = (example.width / 2) - PIXEL_SIZE / 2;
 const DISPLAY_SIZE = 500;
-const FOCAL_LENGTH = 800;
 
 example.width = DISPLAY_SIZE;
 example.height = DISPLAY_SIZE;
 
 ctx.strokeRect(0, 0, DISPLAY_SIZE, DISPLAY_SIZE);
+let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
 
 const OFFSET = 125 + PIXEL_SIZE / 2;
 
@@ -33,7 +33,7 @@ const edge_table = [
 const camera = {
     x: 248,
     y: 248,
-    z: 800
+    z: 2800
 }
 
 const screen = {
@@ -47,6 +47,8 @@ function drawVertices(vertex_table) {
 }
 
 function drawEdges(vertex_table, edge_table) {
+    ctx.beginPath();
+
     for (let edge of edge_table) {
         ctx.moveTo(vertex_table[edge[0]][0] + PIXEL_SIZE / 2, vertex_table[edge[0]][1] + PIXEL_SIZE / 2);
         ctx.lineTo(vertex_table[edge[1]][0] + PIXEL_SIZE / 2, vertex_table[edge[1]][1] + PIXEL_SIZE / 2);        
@@ -56,7 +58,7 @@ function drawEdges(vertex_table, edge_table) {
 }
 
 function getProgectVertex(vertex) {
-     let [x, y, z] = vertex;
+    let [x, y, z] = vertex;
 
     let x_length = (Math.abs(screen.z - z) * Math.abs(x - camera.x)) / (Math.abs(screen.z - z) + Math.abs(camera.z - screen.z))    
     let x_project = x > camera.x ? x - x_length : x + x_length;
@@ -103,7 +105,7 @@ function drawFigure(vertex_table, edge_table) {
     drawEdges(vertex_table, edge_table);
 };
 
-drawFigure(getProgectVertexTable(vertex_table), edge_table);
+drawFigure(getProgectVertexTable(vertex_table), edge_table); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // strokeRect(x, y, ширина, высота) // Рисует прямоугольник
 // fillRect(x, y, ширина, высота)   // Рисует закрашенный прямоугольник
@@ -127,3 +129,19 @@ function multiply(a, b) {
 
   var a = [[8, 3], [2, 4], [3, 6]],
   b = [[1, 2, 3], [4, 6, 8]];
+
+  const range = document.querySelector('#range')
+  const distance = document.querySelector('.distance')
+
+  range.addEventListener('input', function() {
+    camera.z = this.value;
+    distance.innerHTML = this.value;
+    ctx.clearRect(1, 1, 490, 490);
+    drawFigure(getProgectVertexTable(vertex_table), edge_table);
+  })
+
+  const btn = document.querySelector('#btn')
+
+  btn.addEventListener('click', function() {
+    ctx.clearRect(1, 1, 490, 490);
+  })
